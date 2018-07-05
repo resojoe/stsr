f8309b4fb26215a2f8a49d9842f73807b529b0f4
Joseph Heath <jtheath@gmail.com>
2018-07-05 15:27:30 -0700

    completed renaming operation

--- app/src/androidTest/java/com/seetalk/seetalk/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/seetalk/seetalk/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.seetalk.seetalkrecognizer", appContext.getPackageName());
+        assertEquals("com.seetalk.seetalk", appContext.getPackageName());
     }
 }
